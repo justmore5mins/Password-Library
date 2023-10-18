e5c03a63073a8837f95b296b2145237fe9ae3246
Justmore5mins <standby-nexuses@icloud,com>
2023-10-18 16:19:32 +0800

    add

---
+++ front.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("form");
+const username = form.querySelector("input[name='username']").value;
+const passowrd = form.querySelector("input[name='password']").value;
+
