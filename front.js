e1f7965987394f036ff0a655d1300a5615e38f0e
Justmore5mins <standby-nexuses@icloud,com>
2023-10-18 17:01:13 +0800

    add

--- front.js
+++ front.js
@@ -2,9 +2,10 @@ window.onload = () => {
     window.onresize = () => {
         const submitBtn = document.querySelector("#submitBtn");
         submitBtn.addEventListener("click", () => {
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-        console.log("user ",username,"log in by ",password)
+            submitBtn.preventDefault();
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            console.log(username,password);
         });
-      };
-  };    
+    };
+};    
