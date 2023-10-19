b8762f3125e23d27a36ef34cc00cb02d7bb9c174
Justmore5mins <standby-nexuses@icloud,com>
2023-10-19 15:05:39 +0800

    add

--- user.py
+++ user.py
@@ -1,13 +1,31 @@
-import os
+class User():
+    def __init__(self,path, username:str, password:str) -> None:
+        self.path = path
+        self.username = username
+        self.password = password
 
-def CheckUser(data:dict) -> bool:
-    getusers = {str:str}
-    with open("user.login") as f:
-        for line in f:
-            # 跳過空白行
-            if line == "\n":
-                continue
-            username, password = line.split()
-            getusers.update(username,password)
-            # 列印資料
-            print(username, password)
+    def CheckUser(self) -> int:
+        """
+        0:scusses
+        1:no account
+        2:password wrong
+        """
+        users = {}
+        with open(self.path) as f:
+            for line in f:
+                # 跳過空白行
+                if line == "\n":
+                    continue
+                username, password = line.split()
+                users.update(username,password)
+        if self.username in users & users[password] == self.password:
+            
+        else:
+            return 3
+                
+                
+    def AddUser(self):
+        data = f"{self.username} {self.password}"
+        write = open(self.path,"a")
+        write.write(data)
+        write.close()
