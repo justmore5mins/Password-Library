dd4e92aca69b090867b704ca1ae4162627279e62
Justmore5mins <standby-nexuses@icloud,com>
2023-10-24 13:05:26 +0800

    add

--- front/gui.py
+++ front/gui.py
@@ -3,10 +3,16 @@ sys.path.append("D:\Password_Library")
 
 from tkinter import Tk, Entry,Label,Button
 from command import *
-from time import sleep as delay
 
 login = Tk()
 user = User()
+#function defining
+def ReadUserData():
+    global username
+    global password
+    
+    username = username_entry.get()
+    password = password_entry.get()
 
 #window setting
 login.title("Passowrd Library Login")
@@ -22,8 +28,9 @@ password_Text = Label(text="Password")
 password_Text.place(x=65,y=125)
 password_entry = Entry(login,show="*")
 password_entry.place(x=43,y=150,width=120)
-sumbit_btn = Button(text="Log in",command=user.ReadUserData(username=username_entry,password=password_entry))
+sumbit_btn = Button(text="Log in",command=ReadUserData())
 sumbit_btn.place(x=65,y=175)
 
+print(f"user {username} log in by {password}")
 
 login.mainloop()
