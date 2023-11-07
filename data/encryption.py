b0fcb80d066e9765bc761ac4691b77a572cb70dd
Justmore5mins <standby-nexuses@icloud,com>
2023-11-07 17:31:53 +0800

    add

--- data/encryption.py
+++ data/encryption.py
@@ -1,15 +1,12 @@
 from os.path import exists
 from os import makedirs,system,remove
 
-from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
-from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
-from os import urandom
-from base64 import b64decode, b64encode
+from Crypto.Cipher import AES, PKCS1_OAEP
 
 class Encrypt:
-    def __init__(self,username:str,password:str, source:str) -> None:
+    def __init__(self,username:str,password:str) -> None:
         '''
         userdata:
             username:the user name of website
@@ -19,62 +16,55 @@ class Encrypt:
         '''
         self.username = username
         self.password = password
-        self.source = source
         
-    def __prepare(self, dir:str = ""):
+    def encrypt(self, number:int):
         '''
         public key, private key AESKey
         '''
-        AESKey = urandom(16)
         key = RSA.generate(2048)
         pubkey = key.publickey()
-        prikey = key.exportKey()
-
-        with open(f"{dir}/aes.txt","wb") as file:
-            file.write(b64encode(AESKey))
-        with open(f"{dir}/pubkey.pem","wb") as file:
-            file.write(b64encode(pubkey.exportKey()))
-        with open(f"{dir}/prikey.pem","wb") as file:
-            file.write(b64encode(prikey))
-
-        return pubkey, AESKey
+        aeskey = get_random_bytes(16)
         
-    def encrypt(self):
-        """
-        using aes and rsa to encrypt data
-        """
-        pubkey,aeskey = self.__prepare(self.username)
-        rsaencrypt = pubkey
-        aes = AES.new(aeskey,AES.MODE_CBC).encrypt(rsaencrypt)
-        last = b64encode(aes)
+        #rsa encrypt aes key
+        atrsa = PKCS1_OAEP.new(pubkey)
+        encaeskey = atrsa.encrypt(aeskey)
         
-        return last
+        #AES to encrypt data
+        ataes = AES.new(aeskey,AES.MODE_EAX)
+        encrypted, tag = ataes.encrypt_and_digest(bytes(self.password,encoding="utf-8"))
+        #saving files
+        #rsa keys
+        with open(f"{self.username}/pubkey.pem","wb") as file:
+            file.write(key.publickey().export_key(passphrase=self.username,pkcs=8,protection="scryptAndAES128-CBC"))
+        with open(f"{self.username}/prikey.pem","wb") as file:
+            file.write(key.export_key(passphrase=self.username,pkcs=8,protection="scryptAndAES128-CBC"))
+        
+        with open(f"{self.username}/encryption{number}.bin","wb") as file:
+            file.write(encaeskey)
+            file.write(ataes.nonce)
+            file.write(tag)
+            file.write(encrypted)
+        return encrypted
 
 class Decrypt:
-    def __init__(self,username:str,data:str|bytes):
-        #---------------RSA---------------------
-        try:
-            data = bytes(data,encoding="utf-8")
-        except:
-            pass
-        with open(f"{username}/prikey.pem","rb") as file:
-            with open(f"{username}/cache.pem","wb") as f:
-                f.write(b64decode(file.read()))
-        with open(f"{username}/cache.pem","rb") as file:
-            rsakey = rsa.PrivateKey.load_pkcs1(bytes(file.read()))
-        #---------------AES----------------------
-        with open(f"{username}/aes.txt","rb") as file:
-            key = b64decode(file.read())
-        text = AES.new(key,AES.MODE_CBC)
-        aes = text.decrypt(b64decode(pad(data,AES.block_size)))
-        remove(f"{username}/cache.pem")
-        return str(rsa.decrypt(aes,rsakey))
+    def decrypt(username:str, encryption_file:str):
+        prikey = RSA.import_key(open(f"{username}/prikey.pem").read(),passphrase=username)
+        with open(encryption_file,"rb") as file:
+            encaeskey = file.read(prikey.size_in_bytes())
+            nonce = file.read(16)
+            tag = file.read(16)
+            encrypted = file.read()
         
+        #rsa decrypt aes key
+        rsa = PKCS1_OAEP.new(prikey)
+        aeskey = rsa.decrypt(encaeskey)
         
-    
-thing = Encrypt(username="Justmore5mins",password="HelloWorld",source="www.google.com")
-system("cls")
-encrypt = thing.encrypt()
-print(encrypt)
-decrypt = Decrypt("Justmore5mins",encrypt)
-print(decrypt)
+        #aes decrypt data
+        aes = AES.new(aeskey,AES.MODE_EAX,nonce)
+        data = aes.decrypt_and_verify(encrypted,tag)
+        
+        return data.decode()
+system("cls")    
+encrypt = Encrypt(username="Justmore5mins",password="HelloWorld")
+print(encrypt.encrypt(1))
+print(Decrypt.decrypt("Justmore5mins","Justmore5mins/encryption1.bin"))
